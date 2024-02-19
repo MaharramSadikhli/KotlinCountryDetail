880abef8e4489577dc038042bbfceb4133ebf1f9
Muarem <s.muarem10@gmail.com>
2024-02-19 07:37:40 +0400

    created Country Model class

---
+++ app/src/main/java/com/example/kotlincountries/model/CountryModel.kt
@@ -0,0 +1,10 @@
+package com.example.kotlincountries.model
+
+data class CountryModel(
+    val name: String?,
+    val capital: String?,
+    val region: String?,
+    val currency: String?,
+    var flag: String?,
+    val language: String?,
+)
