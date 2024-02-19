f01d4a046062b2385f4ecd9783f1297da8a07717
Muarem <s.muarem10@gmail.com>
2024-02-19 08:14:20 +0400

    country details view model created

---
+++ app/src/main/java/com/example/kotlincountries/viewmodel/DetailsViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.kotlincountries.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.kotlincountries.model.CountryModel
+
+class DetailsViewModel : ViewModel() {
+
+    private val countryLiveData = MutableLiveData<CountryModel>()
+
+    fun getDataFromRoom() {
+        val country = CountryModel(
+            "Turkey",
+            "Asia",
+            "Ankara",
+            "TRY",
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Flag_of_Turkey.svg/2560px-Flag_of_Turkey.svg.png",
+            "Turkish"
+        )
+        countryLiveData.value = country
+    }
+
+}
