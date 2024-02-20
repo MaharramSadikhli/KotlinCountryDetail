d4a5d12a29a9736c326954b83776ef9a20d9f6d1
Muarem <s.muarem10@gmail.com>
2024-02-20 06:30:38 +0400

    update country model class for room db

--- app/src/main/java/com/example/kotlincountries/model/CountryModel.kt
+++ app/src/main/java/com/example/kotlincountries/model/CountryModel.kt
@@ -1,10 +1,31 @@
 package com.example.kotlincountries.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 data class CountryModel(
+    @ColumnInfo(name = "name")
     val name: String?,
+
+    @ColumnInfo(name = "capital")
     val capital: String?,
+
+    @ColumnInfo(name = "region")
     val region: String?,
+
+    @ColumnInfo(name = "currency")
     val currency: String?,
+
+    @ColumnInfo(name = "flag")
     var flag: String?,
+
+    @ColumnInfo(name = "language")
     val language: String?,
-)
+) {
+
+    @PrimaryKey(autoGenerate = true)
+    var uuid: Int = 0
+
+}
