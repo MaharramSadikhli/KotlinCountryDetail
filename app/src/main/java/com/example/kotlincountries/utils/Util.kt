f4a913b07d3d1672ccdad241eea0bf04ba9ce7f7
Muarem <s.muarem10@gmail.com>
2024-02-20 06:24:18 +0400

    created ImageView extension func for pull images

---
+++ app/src/main/java/com/example/kotlincountries/utils/Util.kt
@@ -0,0 +1,19 @@
+package com.example.kotlincountries.utils
+
+import android.widget.ImageView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.kotlincountries.R
+
+
+fun ImageView.downloadFromUrl(url: String?, progressDrawable: CircularProgressDrawable) {
+    val options = RequestOptions()
+        .placeholder(progressDrawable)
+        .error(R.mipmap.ic_launcher_round)
+
+    Glide.with(context)
+        .setDefaultRequestOptions(options)
+        .load(url)
+        .into(this)
+}
