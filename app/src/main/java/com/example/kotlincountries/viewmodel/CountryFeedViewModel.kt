e6484a29454be3b98facefe7c4659942b77a3a8a
Muarem <s.muarem10@gmail.com>
2024-02-20 09:45:58 +0400

    get data from sqlite and from api

--- app/src/main/java/com/example/kotlincountries/viewmodel/CountryFeedViewModel.kt
+++ app/src/main/java/com/example/kotlincountries/viewmodel/CountryFeedViewModel.kt
@@ -1,49 +1,122 @@
 package com.example.kotlincountries.viewmodel
 
+import android.app.Application
+import android.widget.Toast
+import androidx.compose.runtime.Composable
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.kotlincountries.model.CountryDB
 import com.example.kotlincountries.model.CountryModel
+import com.example.kotlincountries.service.ServiceAPI
+import com.example.kotlincountries.utils.SharedPreferencesTime
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
+import kotlinx.coroutines.launch
 
-class CountryFeedViewModel: ViewModel() {
+class CountryFeedViewModel(application: Application): RootViewModel(application) {
+
+    private val serviceApi = ServiceAPI()
+    private val disposable = CompositeDisposable()
+    private var sharedPreferencesTime = SharedPreferencesTime(getApplication())
+    private var refreshTime = 10 * 60 * 1000 * 1000 * 1000L
 
     val countries = MutableLiveData<List<CountryModel>>()
     val error = MutableLiveData<Boolean>()
     val loading = MutableLiveData<Boolean>()
 
     fun refreshData() {
-        val country1 = CountryModel(
-            "Turkey",
-            "Ankara",
-            "Asia",
-            "TRY",
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Flag_of_Turkey.svg/2560px-Flag_of_Turkey.svg.png",
-            "Turkish"
-        )
 
-        val country2 = CountryModel(
-            "Germany",
-            "Berlin",
-            "Europe",
-            "EUR",
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Flag_of_Germany.svg/2560px-Flag_of_Germany.svg.png",
-            "German"
-        )
+        val updateTime = sharedPreferencesTime.getTime()
 
-        val country3 = CountryModel(
-            "Azerbaijan",
-            "Baku",
-            "Asia",
-            "AZN",
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Flag_of_Azerbaijan.svg/2560px-Flag_of_Azerbaijan.svg.png",
-            "Azerbaijani"
-        )
+        if (updateTime != null && updateTime != 0L && System.nanoTime() - updateTime < refreshTime) {
+            // get data from sqlite
+            getDataFromSQLite()
+        } else {
+            // get data from api
+            getDataFromAPI()
+        }
 
 
-        val countryList = arrayListOf<CountryModel>(country1, country2, country3)
 
+
+    }
+
+    fun refreshFromAPI() {
+        // get data from api
+        getDataFromAPI()
+    }
+
+    private fun getDataFromAPI() {
+
+        val service = serviceApi.getData()
+
+        loading.value = true
+
+        disposable
+            .add(
+                service
+                    .subscribeOn(Schedulers.newThread())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribeWith(object: DisposableSingleObserver<List<CountryModel>>() {
+                        override fun onSuccess(t: List<CountryModel>) {
+                            Toast.makeText(getApplication(), "from api", Toast.LENGTH_LONG).show()
+                            // store sqlite
+                            storeDataSQLite(t)
+                        }
+
+                        override fun onError(e: Throwable) {
+                            e.printStackTrace()
+                            error.value = true
+                            loading.value = false
+                        }
+
+                    })
+            )
+
+    }
+
+    private fun getDataFromSQLite() {
+        loading.value = true
+
+        launch {
+            val dao = CountryDB(getApplication()).countryDao()
+            val getCountries = dao.getAllCountries()
+
+            showData(getCountries)
+            Toast.makeText(getApplication(), "from sqlite", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun showData(countryList: List<CountryModel>) {
         countries.value = countryList
         error.value = false
         loading.value = false
+    }
+
+    private fun storeDataSQLite(countryList: List<CountryModel>) {
+        launch {
+            val dao = CountryDB(getApplication()).countryDao()
+            dao.deleteAllCountries()
+            val listLong = dao.insertAll(*countryList.toTypedArray())
+
+            var i = 0
+            while (i < countryList.size) {
+                countryList[i].uuid = listLong[i].toInt()
+                i++
+            }
+
+            showData(countryList)
+        }
+
+        sharedPreferencesTime.saveTime(System.nanoTime())
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+
+        disposable.clear()
     }
 
 }
